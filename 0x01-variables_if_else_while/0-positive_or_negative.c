7a00e499b1cca6bf993b4c7a79c6db15058bf761
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-10 08:49:36 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 /* print whether the number stored is positive or negative */
 {
 if (n > 0)
-printf( "%d is positive\n", n);
+printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
 else
