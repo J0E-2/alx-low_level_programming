9ce9fa3ef6431d97038955dab3aedb88e96d4445
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-11 05:18:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program prints all sinle digit numbers 0f base 10
+ * Return: (0)
+ */
+int main(void)
+{
+int a;
+a = 48;
+while (a <= 57)
+{
+putchar(a);
+a++;
+}
+return (0);
+}
