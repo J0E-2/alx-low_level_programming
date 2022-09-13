8827779564071a93b0e1556255c0bfb2fcf6faf1
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-13 14:45:46 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-aphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @char: parameter
+ * Result: (0)
+ */
+int _putchar(char);
+int main(void)
+{
+int a = 97;
+while (a <= 122)
+{
+_putchar(a);
+a++;
+}
+_putchar(10);
+return (0);
+}
