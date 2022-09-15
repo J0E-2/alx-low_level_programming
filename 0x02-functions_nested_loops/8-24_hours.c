f97843bde78fd510e2c8f8f4520719e588ebd250
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-15 16:50:32 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,8 +11,8 @@ int i;
  int k;
  int l;
  i = 0;
- l = (i % 100);
- while (l < 24)
+ l = 0;
+ while (i < 3 && l < 10 && i * 10 + l < 24)
 	{
 	 j = 0;
 	 while (j < 6)
@@ -20,6 +20,7 @@ int i;
 	     k = 0;
 	  while (k < 10)	    
 	    {
+		 _putchar(i + '0');
 		 _putchar(l + '0');
 		 _putchar(':');
 		 _putchar(j + '0');
@@ -30,5 +31,6 @@ int i;
 	  j++;
 	   }
 	 l++;
+	 i++;
 	}
 }
