d2bc75d3037e01ba9515e80f7866e066481717b4
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-15 11:46:22 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,14 @@ int print_last_digit(int j)
 {
 int i;
 i = (j % 10);
- if (i > 0 || i == 0)
+if (i > 0 || i == 0)
+{
 _putchar(i + '0');
-else
-_putchar(-i + '0');
 return (i);
+}
+else
+{
+_putchar(-i + '0');
+return (-i);
+}
 }
