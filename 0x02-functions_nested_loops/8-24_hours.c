e72e7212bcedb9fdefe5c5bbf9ca1ef3190b85e1
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-15 12:45:12 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@ void jack_bauer(void)
 {
 int a;
 int b;
-a = 0;
+a = '0';
 while (a < 24)
 {
-b = 0;
+b = '0';
 while (b < 60)
 {
-_putchar('0');
+_putchar(a);
 _putchar(':');
-_putchar(b + '0');
+_putchar(b);
 _putchar('\n');
 b++;
 }
