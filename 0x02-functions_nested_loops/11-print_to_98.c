817ca483ab6c63c51a121aa0144c86eace956d2c
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-15 19:20:12 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,20 @@ void print_to_98(int n)
 {
 while (n > 0 && n < 99)
 {
-  if (n < 10 && n > 0)
+if (n < 10 && n > 0)
+  {
 _putchar(n + '0');
 _putchar(',');
 _putchar(' ');
 n++;
+  }
  else
+   {
    _putchar((n / 10) + '0');
  _putchar((n % 10) + '0');
  _putchar(',');
  _putchar(' ');
  n++;
+   }
   }
 }
