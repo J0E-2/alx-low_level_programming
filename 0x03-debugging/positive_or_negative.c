1a7bd2ae079d58eb98a205c964f43fc997e0b80b
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-16 05:06:05 -0700

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Decription: program assigns random numbers each time it is executed
+ * Return: 0
+ */
+void positive_or_negative(int n)  
+{  
+srand(time(0));  
+n = rand() - RAND_MAX / 2;  
+/* print whether the number stored is positive or negative */  
+{    
+if (n > 0)      
+printf("%d is positive\n", n);    
+else if (n == 0)      
+printf("%d is zero\n", n);    
+else      
+printf("%d is negative\n", n);    
+}
+}  
