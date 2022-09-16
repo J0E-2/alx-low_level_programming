850d2f2492864ae1c9bdd7e76687ea6e89a3daa0
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-16 05:11:41 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,17 +6,17 @@
  * Decription: program assigns random numbers each time it is executed
  * Return: 0
  */
-void positive_or_negative(int n)  
+void positive_or_negative(int i)  
 {  
 srand(time(0));  
-n = rand() - RAND_MAX / 2;  
+i = rand() - RAND_MAX / 2;  
 /* print whether the number stored is positive or negative */  
 {    
-if (n > 0)      
-printf("%d is positive\n", n);    
-else if (n == 0)      
-printf("%d is zero\n", n);    
+if (i > 0)      
+printf("%d is positive\n", i);    
+else if (i == 0)      
+printf("%d is zero\n", i);    
 else      
-printf("%d is negative\n", n);    
+printf("%d is negative\n", i);    
 }
 }  
