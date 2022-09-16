58bc39692a46fdb887a0c94d27e10eef0d469272
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-15 19:18:11 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_to_98(int n)
+{
+while (n > 0 && n < 99)
+{
+  if (n < 10 && n > 0)
+_putchar(n + '0');
+_putchar(',');
+_putchar(' ');
+n++;
+ else
+   _putchar((n / 10) + '0');
+ _putchar((n % 10) + '0');
+ _putchar(',');
+ _putchar(' ');
+ n++;
+  }
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,5 @@ int _abs(int j);
 int print_last_digit(int j);
 void jack_bauer(void);
 int add(int, int);
+void print_to_98(int n);
 #endif
