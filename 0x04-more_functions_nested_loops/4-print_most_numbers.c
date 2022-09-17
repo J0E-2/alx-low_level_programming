4a877df7ddbfccfa9497d7bdf30af8c509cdb750
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-16 18:21:59 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,13 @@
  */
 void print_most_numbers(void)
 {
-int i = 0;
-while (i < 10)
+int i;
+for (i = 0; i < 10; i++)
 {
-if (i == 2 || i == 4)
-continue;
-else
+if (i != 2 || i != 4)
 _putchar(i + '0');
-i++;
+else
+continue;
 }
 _putchar('\n');
 }
