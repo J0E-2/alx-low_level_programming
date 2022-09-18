6edff1e70572b8e4f181b1f87323b283984251a7
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-17 19:03:09 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 if ((i % 15) == 0)
 printf("FizzBuzz");
@@ -22,6 +22,7 @@ else
 printf("%d", i);
 printf(" ");
 }
+printf("Buzz");
 printf("\n");
 return (0);
 }
