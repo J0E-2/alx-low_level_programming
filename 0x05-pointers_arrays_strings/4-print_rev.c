3cc57cc2bd9f78d8ecdb468f73870c6166f6dc60
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-20 18:04:29 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,16 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
 		i++;
+		s++;
 	}
 	while (i >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(*s);
 		i--;
+		s--;
 	}
 	_putchar('\n');
 }
