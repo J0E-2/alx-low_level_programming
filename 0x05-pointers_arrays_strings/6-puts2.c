a6417a7eeb627f4a634a8aad9ab25899703cf0a2
Joseph-Ndungu-K <joeke003@gmail.com>
2022-09-23 16:19:49 -0700

    puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,15 +9,10 @@
 void puts2(char *str)
 {
 	int i;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		{
-			_putchar(*(str+i));
-			i++;
-		}
-	}
+
+       	for (i = 0; str[i] != '\0'; i++)
+       		if (i % 2 == 0)
+       			_putchar(str[i]);
 	_putchar('\n');
 }
        	
