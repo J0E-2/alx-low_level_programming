e8c976bf8b1f5e5d46fff1f1b7cac3d27436c907
Joseph-Ndungu-K <joeke003@gmail.com>
2022-10-12 13:11:48 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - structure
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
