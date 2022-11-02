a0534e9955ca4e2c7f74283def998b484dead8a7
Joseph-Ndungu-K <joeke003@gmail.com>
2022-11-02 16:38:26 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+0;276;0c/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to write
+ * to the file
+ *
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, bytes = 0, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[len] != '\0')
+		len++;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+
+	if (text_content != NULL)
+	{
+		bytes = write(fd, text_content, len);
+	}
+
+	if (fd == -1 || bytes == -1)
+		return (-1);
+
+	return (1);
+}
