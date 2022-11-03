79d64a192152d6676332c124699e10febb8f2a93
Joseph-Ndungu-K <joeke003@gmail.com>
2022-11-03 07:38:24 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,12 @@ int create_file(const char *filename, char *text_content)
 		len++;
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	bytes = write(fd, text_content, len);
 
 	if (fd == -1 || bytes == -1)
 		return (-1);
 
+	close(fd);
 	return (1);
 }
