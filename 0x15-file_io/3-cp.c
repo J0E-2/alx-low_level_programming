d18c94e980ad8ac44508382eacae67dd7bb143e0
Joseph-Ndungu-K <joeke003@gmail.com>
2022-11-04 04:16:10 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 int main(int argc, char **argv)
 {
 	char *buf;
-	int fd, fd2, rd, wr, clse, clse2;
+	int fd, fd2, rd = 1024, wr, clse, clse2;
 
 	if (argc != 3)
 	{
@@ -19,17 +19,20 @@ int main(int argc, char **argv)
 	buf = malloc(sizeof(char) * 1024);
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	fd = open(argv[1], O_RDONLY);
-	rd = read(fd, buf, 1024);
-	if (fd == -1 || rd == -1)
+	while (rd == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	wr = write(fd2, buf, rd);
-	if (wr == -1 || fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		rd = read(fd, buf, 1024);
+		if (fd == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(fd2, buf, rd);
+		if (wr == -1 || fd2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	free(buf);
 	clse = close(fd);
