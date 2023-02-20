e78399eefe2bc6103a0bbd4d03a3775f09d3f03a
Joseph Ndungu <joeke003@gmail.com>
2023-02-20 15:54:18 +0300

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,9 +10,6 @@
 #include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-<<<<<<< HEAD
-=======
 int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char **argv);
->>>>>>> bd287b4fc4a4095b6709e971465e11376604743c
 #endif
